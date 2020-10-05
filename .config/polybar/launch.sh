260d087cc8fc3fcd080a38e97a4df59eaf879588
Aseem Athale <athaleaseem@gmail.com>
2020-10-05 10:53:43 +0530

    Switched to mpd.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,5 +8,5 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log 
 polybar top >>/tmp/polybar1.log 2>&1 &
-
+polybar bottom >> /tmp/polybar2.log 2>&1 &
 echo "Bars launched..."
