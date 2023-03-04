7989ae9c0d7e09f216eeb073cdde9fb8128c31dd
Aseem Athale <athaleaseem@gmail.com>
2023-03-04 21:06:09 +0530

    firefox: Disable annoying orange mic/webcam usage popup
    The idea itself is not bad, but it makes sway and Hyprland behave
    weirdly, messing with the window management and tiling,
    and firefox crashes soon after in Hyprland, at least.
    So disable it.
    
    Signed-off-by: Aseem Athale <athaleaseem@gmail.com>

--- .local/share/firefox/user-overrides.js
+++ .local/share/firefox/user-overrides.js
@@ -1,3 +1,4 @@
 user_pref("keyword.enabled", true);
 user_pref("media.ffmpeg.vaapi.enabled", true);
 user_pref("media.ffvpx.enabled", false);
+user_pref("privacy.webrtc.legacyGlobalIndicator", false);
