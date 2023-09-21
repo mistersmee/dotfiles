8c3273cc4189ac8d301df9d6e0e89edaf6009173
Aseem Athale <athaleaseem@gmail.com>
2023-09-21 16:59:21 +0530

    firefox: Remove un-needed options
    Signed-off-by: Aseem Athale <athaleaseem@gmail.com>

--- .local/share/firefox/haxxor_clg_user-overrides.js
+++ .local/share/firefox/haxxor_clg_user-overrides.js
@@ -1,4 +1,3 @@
 user_pref("keyword.enabled", true);
 user_pref("media.ffmpeg.vaapi.enabled", true);
-user_pref("media.ffvpx.enabled", false);
 user_pref("privacy.webrtc.legacyGlobalIndicator", false);
--- .local/share/firefox/normie_user-overrides.js
+++ .local/share/firefox/normie_user-overrides.js
@@ -1,8 +1,6 @@
 user_pref("keyword.enabled", true);
 user_pref("media.ffmpeg.vaapi.enabled", true);
-user_pref("media.ffvpx.enabled", false);
 user_pref("privacy.webrtc.legacyGlobalIndicator", false);
-user_pref("media.eme.enabled", true);
 /* override recipe: RFP is not for me ***/
 user_pref("privacy.resistFingerprinting", false); // 4501
 user_pref("privacy.resistFingerprinting.letterboxing", false); // 4504 [pointless if not using RFP]
